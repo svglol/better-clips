8c3b88e78665ffeda93ad3cd01be353672c4522a
Matthew Trotman <savageguy@gmail.com>
2024-09-06 23:04:32 +1200

    test: add tests

--- tests/nuxt/app.spec.ts
+++ tests/nuxt/app.spec.ts
@@ -7,8 +7,8 @@ describe('app', () => {
     const component = await mountSuspended(app, { route: '/' })
     expect(component).toBeTruthy()
   })
-  it('app has welcome to nuxt text', async () => {
+  it('app has Better Twitch Clips Browser text', async () => {
     const component = await mountSuspended(app, { route: '/' })
-    expect(component.html()).toContain('Welcome to Nuxt!')
+    expect(component.html()).toContain('Better Twitch Clips Browser')
   })
 })
