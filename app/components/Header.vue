360b6b4ea289d49518335060df1136c321cb9d8a
Matthew Trotman <savageguy@gmail.com>
2024-09-07 19:47:34 +1200

    feat: adjust header size

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row items-center justify-between gap-4 bg-gray-100 px-2 py-8 shadow dark:bg-gray-900">
+  <div class="flex flex-row items-center justify-between gap-4 bg-gray-100 px-2 py-4 shadow dark:bg-gray-900">
     <NuxtLink to="/" class="flex flex-row items-center gap-4 text-sm text-black hover:opacity-80 sm:text-xl dark:text-white">
       🎬 Better Twitch Clips
     </NuxtLink>
