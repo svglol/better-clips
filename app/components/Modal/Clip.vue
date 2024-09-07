7f4d45b8a99cd9d0201424609d080b3a3986f291
Matthew Trotman <savageguy@gmail.com>
2024-09-07 19:26:52 +1200

    feat: add share/download button to clip modal

--- app/components/Modal/Clip.vue
+++ app/components/Modal/Clip.vue
@@ -1,12 +1,22 @@
 <template>
   <UModal :ui="{ width: 'sm:!max-w-[calc(100vw-12rem)] lg:!max-w-[calc(100vw-10vw)] 2xl:!max-w-[calc(100vw-30vw)] xl:m-20 h-auto', container: '!items-center' }">
-    <div class="aspect-w-16 aspect-h-9 relative w-full">
-      <iframe
-        :src="iframeSrc"
-        class="absolute inset-0 size-full"
-        allowfullscreen
-        style="border: none;"
-      />
+    <div class="flex flex-col">
+      <div class="aspect-w-16 aspect-h-9 relative w-full">
+        <iframe
+          :src="iframeSrc"
+          class="absolute inset-0 size-full"
+          allowfullscreen
+          style="border: none;"
+        />
+      </div>
+      <div class="flex flex-row-reverse gap-4 p-4">
+        <UTooltip text="Download">
+          <UButton icon="i-heroicons-arrow-down-tray" color="primary" class="flex-none" :to="videoUrl" _target="_blank" size="sm" variant="ghost" />
+        </UTooltip>
+        <UTooltip text="Share">
+          <UButton icon="i-heroicons-share" color="primary" class="flex-none" size="sm" variant="ghost" @click="startShare" />
+        </UTooltip>
+      </div>
     </div>
   </UModal>
 </template>
@@ -16,12 +26,15 @@ const props = defineProps<{
   id: string
 }>()
 
+const clip = ref<TwitchClip | undefined>()
+const videoUrl = computed(() => clip.value?.thumbnail_url.replace('-preview-480x272.jpg', '.mp4'))
+
 const { data } = await useFetch<TwitchAPIResponse<TwitchClip>>(`/api/twitch/clips/${props.id}`)
 if (data.value && data.value.data && data.value.data.length > 0) {
-  const clip = data.value.data[0]
-  if (clip) {
+  clip.value = data.value.data[0]
+  if (clip.value) {
     useSeoMeta({
-      title: `${clip.title} - ${clip.broadcaster_name}`,
+      title: `${clip.value.title} - ${clip.value.broadcaster_name}`,
     })
   }
 }
@@ -29,4 +42,13 @@ const iframeSrc = computed(() => {
   const { hostname } = new URL(window.location.href)
   return `https://clips.twitch.tv/embed?clip=${props.id}&parent=${hostname}&autoplay=true`
 })
+
+const { share } = useShare()
+
+function startShare() {
+  share({
+    title: document.title,
+    url: location.href,
+  })
+}
 </script>
