6770c56ef4e288bf0911a0670ef08e1af3a3e2c6
Matthew Trotman <savageguy@gmail.com>
2024-09-09 20:28:10 +1200

    feat: added tabs on front page for future expansion

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="flex flex-col gap-6 p-4">
+  <div class="flex flex-col gap-6 p-2 py-0">
     <div v-if="loggedIn" class="flex flex-col gap-6">
-      <!-- Top Clips Section can be added here if needed -->
-      <div v-if="data?.length ?? 0 > 0" class="flex flex-col gap-2">
-        <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-200">
-          Followed Channels
-        </h2>
-        <div class="grid grid-cols-2 items-center gap-4 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-12">
-          <div v-for="channel in data" :key="channel.id">
-            <TwitchFavouriteChannel :channel="channel" />
+      <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" :ui="{ base: 'text-lg' }" />
+      <UTabs
+        v-model="selected"
+        :items="tabs"
+        :default-index="0"
+        :ui="{
+          wrapper: 'space-y-0',
+          list: {
+            base: 'hidden',
+          },
+        }"
+        class="w-full"
+      >
+        <template #followed-channels>
+          <div v-if="data?.length ?? 0 > 0" class="flex flex-col gap-2">
+            <div class="grid grid-cols-2 items-center gap-4 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-12">
+              <div v-for="channel in data" :key="channel.id">
+                <TwitchFavouriteChannel :channel="channel" />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </template>
+      </UTabs>
     </div>
     <div v-else class="flex flex-col items-center justify-center gap-6 py-24">
       <span class="text-center text-lg font-semibold text-gray-900 dark:text-gray-200">
@@ -34,4 +46,43 @@ useSeoMeta({
 })
 
 const { data } = await useFetch<TwitchUser[]>('/api/twitch/user/followed')
+
+const route = useRoute()
+const router = useRouter()
+
+const tabs = ref([
+  {
+    slot: 'followed-channels',
+    id: 'followed-channels',
+    label: 'Followed Channels',
+    active: false,
+    click: () => {},
+  },
+])
+
+const selected = computed({
+  get() {
+    const index = tabs.value.findIndex(item => item.id === route.query.tab)
+    if (index === -1)
+      return 0
+
+    return index
+  },
+  set(value) {
+    if (tabs.value[value]) {
+      router.replace({
+        query: { tab: tabs.value[value].id },
+        hash: '',
+      })
+    }
+  },
+})
+
+const links = computed(() => {
+  return tabs.value.map((tab, index) => ({
+    ...tab,
+    active: selected.value === index,
+    click: () => selected.value = index,
+  }))
+})
 </script>
