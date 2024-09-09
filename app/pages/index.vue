f3196a747226a77632538f795471fd82354b6ba9
Matthew Trotman <savageguy@gmail.com>
2024-09-09 19:45:33 +1200

    feat: small adjustment to front page layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,8 @@
   <div class="flex flex-col gap-6 p-4">
     <div v-if="loggedIn" class="flex flex-col gap-6">
       <!-- Top Clips Section can be added here if needed -->
-      <div v-if="data?.length ?? 0 > 0" class="flex flex-col gap-6">
-        <h2 class="text-3xl font-extrabold text-gray-800 dark:text-gray-200">
+      <div v-if="data?.length ?? 0 > 0" class="flex flex-col gap-2">
+        <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-200">
           Followed Channels
         </h2>
         <div class="grid grid-cols-2 items-center gap-4 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-12">
