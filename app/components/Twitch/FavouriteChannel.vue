3916bbfaa299b71e7ccffec61a90dc7ce4c1c226
Matthew Trotman <savageguy@gmail.com>
2024-09-09 13:38:41 +1200

    fix: text color being wrong for light mode

--- app/components/Twitch/FavouriteChannel.vue
+++ app/components/Twitch/FavouriteChannel.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink :to="`/channel/${channel?.login}`" class="flex flex-row items-center gap-2 rounded bg-gray-100 p-2 shadow transition-all hover:scale-[97%] hover:opacity-80 dark:bg-gray-900">
     <UAvatar :src="channel?.profile_image_url" class="flex-none rounded-full" />
-    <span class="text-primary-800 overflow-hidden text-ellipsis dark:text-gray-200">{{ channel?.display_name }}</span>
+    <span class="overflow-hidden text-ellipsis text-gray-800 dark:text-gray-200">{{ channel?.display_name }}</span>
   </NuxtLink>
 </template>
 
