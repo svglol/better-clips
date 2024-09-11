67b2c377f6c15609563fad796d62a0652da56046
Matthew Trotman <savageguy@gmail.com>
2024-09-12 11:55:53 +1200

    feat: improve caching strategy on top clips

--- server/api/twitch/user/topclips.ts
+++ server/api/twitch/user/topclips.ts
@@ -7,26 +7,36 @@ const querySchema = z.object({
   limit: z.string().default('50'),
 })
 
-export default defineEventHandler(async (event) => {
-  const query = await getValidatedQuery(event, querySchema.parse)
-  const page = Number(query.page)
-  const limit = Number(query.limit)
-  await refreshTwitchoAuthToken(event)
-  const session = await getUserSession(event)
-  if (!session.user) {
-    return createError({ statusCode: 401, statusMessage: 'Unauthorized' })
-  }
-  const followedChannels = await getFollowedChannels(session)
-  let allClips = await getAllClipsFromFollowedChannels(followedChannels, session)
-  allClips = allClips.filter(clip => clip.view_count > 50)
+const getAllClipsFromFollowedChannels = defineCachedFunction(async (channels: TwitchFollowedChannel[], session: UserSession) => {
+  const clipPromises = channels.map(async (channel) => {
+    const params = new URLSearchParams()
+    params.append('broadcaster_id', channel.broadcaster_id)
+    params.append('first', '5')
 
-  const maxMinutesSince = Math.max(...allClips.map(clip => differenceInMinutes(new Date(), new Date(clip.created_at))))
+    const now = new Date()
+    now.setMinutes(now.getMinutes() >= 30 ? 30 : 0, 0, 0)
+
+    const endedAt = now.toISOString()
+    params.append('ended_at', endedAt)
+
+    const startedAt = sub(now, { hours: 24 })
+    startedAt.setMinutes(now.getMinutes() >= 30 ? 30 : 0, 0, 0)
+    params.append('started_at', startedAt.toISOString())
+    const response = await fetchFromTwitchAPI<TwitchClip[]>('/clips', params, session.user?.token.access_token)
+    return response.data.flat()
+  })
+
+  const clipsArrays = await Promise.all(clipPromises)
+  let clips = clipsArrays.flat()
+  clips = clips.filter(clip => clip.view_count > 50)
+
+  const maxMinutesSince = Math.max(...clips.map(clip => differenceInMinutes(new Date(), new Date(clip.created_at))))
   const weightViews = 0.4
   const weightRecency = 0.6
 
   const today = new Date()
 
-  allClips = allClips.sort((a, b) => {
+  clips = clips.sort((a, b) => {
     const viewsA = Math.log10(a.view_count + 1)
     const viewsB = Math.log10(b.view_count + 1)
 
@@ -41,6 +51,25 @@ export default defineEventHandler(async (event) => {
 
     return scoreB - scoreA
   })
+
+  return clips
+}, {
+  name: 'all-clips-from-followed-channels',
+  maxAge: 60 * 60,
+})
+
+export default defineEventHandler(async (event) => {
+  const query = await getValidatedQuery(event, querySchema.parse)
+  const page = Number(query.page)
+  const limit = Number(query.limit)
+  await refreshTwitchoAuthToken(event)
+  const session = await getUserSession(event)
+  if (!session.user) {
+    return createError({ statusCode: 401, statusMessage: 'Unauthorized' })
+  }
+  const followedChannels = await getFollowedChannels(session)
+  const allClips = await getAllClipsFromFollowedChannels(followedChannels, session)
+
   const totalClips = allClips.length
   const totalPages = Math.ceil(totalClips / limit)
   const startIndex = (page - 1) * limit
@@ -62,26 +91,3 @@ export default defineEventHandler(async (event) => {
     pagination: paginationMeta,
   }
 })
-
-async function getAllClipsFromFollowedChannels(channels: TwitchFollowedChannel[], user: UserSession) {
-  const clipPromises = channels.map(async (channel) => {
-    const params = new URLSearchParams()
-    params.append('broadcaster_id', channel.broadcaster_id)
-    params.append('first', '5')
-
-    const now = new Date()
-    now.setMinutes(now.getMinutes() >= 30 ? 30 : 0, 0, 0)
-
-    const endedAt = now.toISOString()
-    params.append('ended_at', endedAt)
-
-    const startedAt = sub(now, { hours: 24 })
-    startedAt.setMinutes(now.getMinutes() >= 30 ? 30 : 0, 0, 0)
-    params.append('started_at', startedAt.toISOString())
-    const response = await fetchFromTwitchAPI<TwitchClip[]>('/clips', params, user.user?.token.access_token)
-    return response.data.flat()
-  })
-
-  const clipsArrays = await Promise.all(clipPromises)
-  return clipsArrays.flat()
-}
