9adfde9832995028572bc7dcdbca633e4c61fa9f
Matthew Trotman <savageguy@gmail.com>
2024-09-25 16:05:18 +1200

    refactor: move token validation and refresh logic to getUserToken

--- server/utils/twitch.ts
+++ server/utils/twitch.ts
@@ -1,6 +1,6 @@
+import type { UserSession } from '#auth-utils'
 import type { H3Event } from 'h3'
 import { hash } from 'ohash'
-import type { UserSession } from '#auth-utils'
 
 export const fetchFromTwitchAPI = defineCachedFunction(async <T>(event: H3Event, endpoint: string, params: URLSearchParams) => {
   const userToken = await getUserToken(event)
@@ -51,12 +51,88 @@ async function getToken() {
   return data
 }
 
+const validateToken = defineCachedFunction(async (event: H3Event, accessToken: string) => {
+  try {
+    const response = await $fetch('https://id.twitch.tv/oauth2/validate', {
+      method: 'GET',
+      headers: {
+        Authorization: `OAuth ${accessToken}`,
+      },
+    })
+    return !!response
+  }
+  catch (error) {
+    console.error('Error validating token:', error)
+    return false
+  }
+}, {
+  maxAge: 60,
+  swr: false,
+  name: 'validate-token',
+  getKey: (event: H3Event, accessToken: string) => hash(accessToken),
+})
+
 export async function getUserToken(event: H3Event) {
   const session = await getUserSession(event)
-  if (!session.user) {
+  if (session.token) {
+    const now = new Date()
+    const expirationDate = new Date(session.token.expires_at)
+
+    const isValid = await validateToken(event, session.token.access_token)
+
+    if (!isValid || expirationDate < now) {
+      const storage = useStorage('data')
+      const isRefreshing = await storage.getItem(`isRefreshing:${session.user?.id}`) || false
+
+      if (!isRefreshing) {
+        await storage.setItem(`isRefreshing:${session.user?.id}`, true)
+        try {
+          const body = new URLSearchParams()
+          body.append('client_id', useRuntimeConfig().twitchClientId)
+          body.append('client_secret', useRuntimeConfig().twitchClientSecret)
+          body.append('refresh_token', session.token.refresh_token ?? '')
+          body.append('grant_type', 'refresh_token')
+
+          const data = await $fetch<{ access_token: string, refresh_token: string, expires_in: number }>('https://id.twitch.tv/oauth2/token', {
+            method: 'POST',
+            body: body.toString(),
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          })
+          if (data && session.user) {
+            session.token.expires_at = Date.now() + (data.expires_in * 1000)
+            session.token.refresh_token = data.refresh_token
+            session.token.access_token = data.access_token
+            await setUserSession(event, session)
+            return session.token
+          }
+          else {
+            await clearUserSession(event)
+            return null
+          }
+        }
+        catch (error) {
+          console.error('Error refreshing oAuth token:', error)
+          await clearUserSession(event)
+        }
+        finally {
+          await storage.setItem(`isRefreshing:${session.user?.id}`, false)
+        }
+      }
+      else {
+        while (await storage.getItem(`isRefreshing:${session.user?.id}`)) {
+          await new Promise(resolve => setTimeout(resolve, 100))
+        }
+        const updatedSession = await getUserSession(event)
+        return updatedSession.token
+      }
+    }
+  }
+  else {
+    await clearUserSession(event)
     return null
   }
-  return session.token
 }
 
 export const getFollowedChannels = defineCachedFunction(async (event: H3Event, session: UserSession) => {
