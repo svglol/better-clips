8d34a18d916fe831fcec166f6ef09f379e37e781
Matthew Trotman <savageguy@gmail.com>
2025-02-26 17:05:06 +1300

    fix(auth): set maxAge for Twitch OAuth tokens to 7 days

--- server/routes/auth/twitch.get.ts
+++ server/routes/auth/twitch.get.ts
@@ -19,7 +19,7 @@ export default oauthTwitchEventHandler({
         refresh_token: tokens.refresh_token,
         expires_at: Date.now() + (tokens.expires_in * 1000),
       },
-    })
+    }, { maxAge: 60 * 60 * 24 * 7 })
     const callbackUrl = getCookie(event, 'callbackUrl')
     deleteCookie(event, 'callbackUrl')
     return sendRedirect(event, callbackUrl ?? '/')
