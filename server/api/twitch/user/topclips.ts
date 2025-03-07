ac2dce2e383786fa2ba6d0c11209c22c3707b744
Matthew Trotman <savageguy@gmail.com>
2025-03-07 19:23:20 +1300

    feat: add limit to number of clip api calls at the same time

--- server/api/twitch/user/topclips.ts
+++ server/api/twitch/user/topclips.ts
@@ -1,6 +1,7 @@
 import type { UserSession } from '#auth-utils'
 import type { H3Event } from 'h3'
 import { differenceInMinutes, sub } from 'date-fns'
+import pLimit from 'p-limit'
 import { z } from 'zod'
 
 const querySchema = z.object({
@@ -17,18 +18,21 @@ const getAllClipsFromFollowedChannels = defineCachedFunction(async (event: H3Eve
     const now = new Date()
     const minutes = now.getMinutes()
     now.setMinutes(minutes < 30 ? 0 : 30, 0, 0)
-    const clips = await Promise.all(channels.map(async (channel) => {
-      const params = new URLSearchParams({
-        broadcaster_id: channel.broadcaster_id,
-        first: '20',
-        ended_at: now.toISOString(),
-        started_at: sub(now, { hours: 24 }).toISOString(),
-      })
+    const limit = pLimit(25)
+    const clips = await Promise.all(
+      channels.map(channel =>
+        limit(async () => {
+          const params = new URLSearchParams({
+            broadcaster_id: channel.broadcaster_id,
+            first: '20',
+            started_at: sub(now, { hours: 24 }).toISOString(),
+          })
 
-      const response = await fetchFromTwitchAPI<TwitchClip>(event, '/clips', params)
-      return response.data
-    }))
-
+          const response = await fetchFromTwitchAPI<TwitchClip>(event, '/clips', params)
+          return response.data
+        }),
+      ),
+    )
     const filteredClips = clips.flat().filter(clip => clip.view_count > 50)
 
     const today = new Date()
