13f4cc9d2d3d881407930b89917c4d07e795ca8b
Matthew Trotman <savageguy@gmail.com>
2025-03-10 19:58:54 +1300

    feat: if a channel has no clips ignore it for 3 hours to speed up trending clips api call

--- server/api/twitch/user/topclips.ts
+++ server/api/twitch/user/topclips.ts
@@ -13,7 +13,20 @@ type QuerySchema = z.infer<typeof querySchema>
 
 const getAllClipsFromFollowedChannels = defineCachedFunction(async (event: H3Event, session: UserSession) => {
   try {
-    const channels = await getFollowedChannels(event, session)
+    let channels = await getFollowedChannels(event, session)
+
+    // Filter out channels that have been ignored
+    const filteredChannels = await Promise.all(channels.map(async (channel) => {
+      const item = await useStorage('cache').getItem(`clips-ignore/${channel.broadcaster_id}`) as { value: boolean, expiresAt: number }
+      if (item && item.expiresAt && item.expiresAt > Date.now()) {
+        return null
+      }
+      if (item && item.expiresAt && item.expiresAt < Date.now()) {
+        await useStorage('cache').removeItem(`clips-ignore/${channel.broadcaster_id}`)
+      }
+      return channel
+    }))
+    channels = filteredChannels.filter(channel => channel !== null)
 
     const now = new Date()
     const minutes = now.getMinutes()
@@ -29,11 +42,24 @@ const getAllClipsFromFollowedChannels = defineCachedFunction(async (event: H3Eve
           })
 
           const response = await fetchFromTwitchAPI<TwitchClip>(event, '/clips', params)
-          return response.data
+          return {
+            id: channel.broadcaster_id,
+            clips: response.data,
+          }
         }),
       ),
     )
-    const filteredClips = clips.flat().filter(clip => clip.view_count > 50)
+
+    // if channel has no clips - add to kv cache to ignore it for 3 hours to speed up processing and reduce api calls
+    const empty = clips.filter(clip => clip.clips.length === 0)
+    for (const clip of empty) {
+      await useStorage('cache').setItem(`clips-ignore/${clip.id}`, {
+        value: true,
+        expiresAt: Date.now() + (10800 * 1000),
+      })
+    }
+
+    const filteredClips = clips.flatMap(channel => channel.clips).filter(clip => clip.view_count > 50)
 
     const today = new Date()
     const maxMinutesSince = Math.max(...filteredClips.map(clip => differenceInMinutes(today, new Date(clip.created_at))))
