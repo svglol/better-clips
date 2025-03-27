d43ecd3392454d79e3362e402b1b7b9c7209baa9
Matthew Trotman <savageguy@gmail.com>
2025-03-27 23:13:17 +1300

    fix: add padding to UILoading component in ClipContainer for better spacing

--- app/components/Clip/Container.vue
+++ app/components/Clip/Container.vue
@@ -23,7 +23,7 @@
           <div v-if="status === 'pending' && clips.length === 0" class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             <TwitchClipSkeleton v-for="i in 24" :key="i" />
           </div>
-          <UILoading v-else-if="status === 'pending'" />
+          <UILoading v-else-if="status === 'pending'" class="p-8" />
           <div v-else-if="clips.length === 0" class="py-auto my-auto flex size-full flex-col items-center justify-center">
             <UIcon name="mdi:robot-confused" class="text-primary-500 dark:text-primary-400 text-9xl" />
             <span class="text-2xl text-gray-500 dark:text-gray-400">No clips found!</span>
